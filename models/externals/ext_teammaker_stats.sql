f99675fd0bbec18628ffc7709e348f2483e538c3
Daniël Laurentius <daniellaurentius@hotmail.com>
2025-12-11 13:01:09 +0000

    Added external layer for the teammaker sheet

---
+++ models/externals/ext_teammaker_stats.sql
@@ -0,0 +1,17 @@
+select
+
+    -- IDs
+    player_id,
+    discord_id,
+
+    -- Properties
+    player_name,
+    event_type,
+    
+    -- Measures
+    average_points,
+    average_placement,
+    events_played_3m,
+    events_played_6m
+
+from {{ ref('agg_player_stats') }}
